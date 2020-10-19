d33f57831321dd31c57ce7642b84531e4210479e
Denson Abraham <densonabraham98@gmail.com>
2020-10-19 16:20:13 +0530

    Integrated prettier with Husky

--- src/controllers/taskbotic.controller.ts
+++ src/controllers/taskbotic.controller.ts
@@ -4,5 +4,5 @@ import { TaskBoticService } from "../services/taskbotic.service";
 const taskboticService = new TaskBoticService();
 
 export async function getParticipantsBatchMapping(req: Request, res: Response) {
-    return taskboticService.getParticipantsBatchMapping(req, res);
+  return taskboticService.getParticipantsBatchMapping(req, res);
 }
--- src/services/taskbotic.service.ts
+++ src/services/taskbotic.service.ts
@@ -1,26 +1,23 @@
 import { Request, Response } from "express";
 import { connect } from "../database.mysql/database";
 import { ParticipantBatchMap } from "../interface/taskbotic";
-const otpGenerator = require('otp-generator') as any;
+const otpGenerator = require("otp-generator") as any;
 
 export class TaskBoticService {
-    constructor() { }
+  constructor() {}
 
-    async getParticipantsBatchMapping(req: Request, res: Response) {
-        try {
-            let db: any = req.headers.db;
-            const conn = await connect(db);
-            let selectQuery:any =`select * from participants_batch_map`
-            const participants = await conn.query(selectQuery);
-            res.status(200).json(
-                participants[0]
-            );
-            return conn.end();
-        }
-        catch (err) {
-            res.status(500).json({
-                message: err
-            })
-        }
+  async getParticipantsBatchMapping(req: Request, res: Response) {
+    try {
+      let db: any = req.headers.db;
+      const conn = await connect(db);
+      let selectQuery: any = `select * from participants_batch_map`;
+      const participants = await conn.query(selectQuery);
+      res.status(200).json(participants[0]);
+      return conn.end();
+    } catch (err) {
+      res.status(500).json({
+        message: err,
+      });
     }
+  }
 }
