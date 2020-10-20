1daad0136eedec198979d7ebb28c380af6d7db65
Denson Abraham <densonabraham98@gmail.com>
2020-10-20 11:59:54 +0530

    API call

--- src/services/participant_master.service.ts
+++ src/services/participant_master.service.ts
@@ -1,43 +1,32 @@
 import { Request, Response } from "express";
 import { connect } from "../database.mysql/database";  
 import * as http from 'http'
-const otpGenerator = require('otp-generator') as any; 
+import * as request from "request-promise-native"; 
+import { json } from "body-parser";
+const otpGenerator = require('otp-generator') as any;  
+let spreadsheetdata : any ;
 
 export class ParticipantMasterService{ 
     constructor() { }
 
-    async writeParticipantTable(req: Request, res: Response) {  
-        let spreadsheetdata:any = http.get('http://localhost:8000/techboutique/taskbotic/getSpreadsheetData')
-        console.log(spreadsheetdata) 
-
+    async writeParticipantTable(req: Request, res: Response) {   
+      try{  
+        const baseUrl = 'http://localhost:8000';
+        const queryString = '/techboutique/taskbotic/getSpreadsheetData';
         var options = {
-            host: "localhost",
-            port: 8000,
-            path: "/techboutique/taskbotic/getSpreadsheetData",
-            method: 'GET'
-          };
-          
-          http.request(options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-              console.log('BODY: ' + chunk);
-            });
-          }).end(); 
-          
-        // try {
-        //     let db: any = req.headers.db;
-        //     const conn = await connect(db);
-        //     let insertQuery:any =`insert into table master_participant_table values`
-            
-        //     res.status(200).json(
-        //         participants[0]
-        //     );
-        //     return conn.end();
-        // }
-        // catch (err) {
-        //     res.status(500).json({
-        //         message: err
-        //     })
-        // }
+            uri: baseUrl + queryString,
+        };
+
+        let result = await request.get(options);  
+        result = JSON.parse(result) 
+        console.log(result)  
+        res.status(200).json(
+          result
+      );
+      } catch (err) {
+        res.status(500).json({
+          message: err,
+        });      
+  } 
 } 
 }
